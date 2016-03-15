bb78735d9fb0b33c497d8cbdfd943904308ffbd7
Tyler Bonnell <tbonnell@uw.edu>
2016-03-15 01:14:54 -0700

    queries the top 5 posts in EarthPorn

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 window.onload = function() {
   document.onkeydown = addKeyToArray;
+  // Fetch the 5 hottest posts on /r/awww
+  loadBG();
+}
+
+// Loads the background from reddit
+function loadBG() {
+  reddit.top('EarthPorn').t('day').limit(5).fetch(function(res) {
+    // res contains JSON parsed response from Reddit
+    console.log(res);
+  });
 }
 
 var keys = [];
-var pass = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+var pass = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]; // ;)
 function addKeyToArray(e) {
   if (e.keyCode == 38 && keys.length != 1) {  // start the sequence over
     keys = [38];
@@ -14,7 +24,6 @@ function addKeyToArray(e) {
     }
   }
 }
-
 function checkCode() {
   if (keys.length == pass.length) {
     for (var i = 0; i < pass.length; i++) {
